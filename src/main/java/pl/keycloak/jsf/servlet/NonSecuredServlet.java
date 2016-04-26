cb49606b19ed3ec920d5dcdc912c33fd37c3a5ce
Mariusz Batyra <mbatyra@wp.eu>
2016-04-26 19:29:00 +0200

    Sample servlet

---
+++ src/main/java/pl/keycloak/jsf/servlet/NonSecuredServlet.java
@@ -0,0 +1,29 @@
+package pl.keycloak.jsf.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Mariusz Batyra
+ */
+public class NonSecuredServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+    private static final Logger LOG = Logger.getLogger(NonSecuredServlet.class.getName());
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.log(Level.INFO, "Access to non secured servlet granted!");
+        
+        PrintWriter out = response.getWriter();
+        out.print("Content of non secured servlet");
+    }
+
+}
