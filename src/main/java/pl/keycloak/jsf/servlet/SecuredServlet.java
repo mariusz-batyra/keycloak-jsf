42eadf55f068f9da3f15836d8d211c9bcc4de743
Mariusz Batyra <mbatyra@wp.eu>
2016-04-26 20:34:27 +0200

    Information from JWT about logged user

--- src/main/java/pl/keycloak/jsf/servlet/SecuredServlet.java
+++ src/main/java/pl/keycloak/jsf/servlet/SecuredServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.representations.AccessToken;
 
 /**
  *
@@ -22,8 +24,16 @@ public class SecuredServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.log(Level.INFO, "Access to secured servlet granted!");
         
+        AccessToken accessToken = ((KeycloakSecurityContext) request.getAttribute(KeycloakSecurityContext.class.getName())).getToken();
+
+        String issuer = accessToken.getIssuer();
+        String username = accessToken.getPreferredUsername();
+
+        LOG.log(Level.INFO, "Token Issuer: {0}", issuer);
+        LOG.log(Level.INFO, "username: {0}", username);
+        
         PrintWriter out = response.getWriter();
-        out.print("Content of secured servlet");
+        out.print("Content of secured servlet\nUsername: " + username);
     }
 
 }
